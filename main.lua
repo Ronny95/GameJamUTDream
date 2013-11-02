61fba612d60650909784933415f24c1d21a31644
RyFry <ryan.lorey@utexas.edu>
2013-11-01 23:53:40 -0500

    Tiles working, still need to fix Player not loading

--- main.lua
+++ main.lua
@@ -7,6 +7,24 @@ local app = {}
 
 RealMap = Map:extend
 {
+	spriteHeight = 32,
+	spriteWidth = 32,
+
+	sprites = { 
+		[1] = Tile:new{ 
+			width = 32, 
+			height = 32,
+			image = 'spritesheet.png',
+			imageOffset = { x = 0, y = 0 } 
+		},
+		[2] = Tile:new{
+			width = 32,
+			height = 32,
+			image = 'spritesheet.png',
+			imageOffset = { x = 32, y = 0}
+		}	 
+	},
+	
 	onNew = function(self)
 		self:loadMap('realmap.txt')
 	end
@@ -14,6 +32,9 @@ RealMap = Map:extend
 
 DreamMap = Map:extend
 {
+	spriteHeight = 32,
+	spriteWidth = 32,
+
 	onNew = function(self)
 		self:loadMap('dreammap.txt')
 	end
@@ -23,7 +44,8 @@ Player = Animation:extend
 {
 	width = 32,
 	height = 32,
-	image = 'player.png',
+	image = 'spritesheet.png',
+	imageOffset = { x = 0, y= 0 },
 	sequences = 
 	{
 		right = { frames = { 1 }, fps = 10},
@@ -60,6 +82,7 @@ Player = Animation:extend
             self.velocity.y = 0
         end
     end,
+
 	onCollide = function(self, other)
 		-- doesn't do anything yet
 	end,
@@ -70,14 +93,13 @@ the.app = App:new
 	onRun = function (self)
 		self.player = Player:new{ x = 0, y = 0 }
 		self:add(self.player)
-		self.map = GameMap:new{}
---		self.view = GameView:new{}
+		self.map = RealMap:new{}
+		self:add(self.map)
 	end,
 
 	onUpdate = function(self)
 		-- doesn't do anything yet
 	end
-
 }
 
 
