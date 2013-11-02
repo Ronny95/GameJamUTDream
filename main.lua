56037838cc17e22392e28e9eb2dc87dab0465e3c
RyFry <ryan.lorey@utexas.edu>
2013-11-02 15:08:17 -0500

    small changes

--- main.lua
+++ main.lua
@@ -195,9 +195,9 @@ end
 
 function updateSound()
 	if music['real'].audio:isLooping() and inGhost then
-		fadeSound(music['real'].audio)
+		fadeSound(music['real'])
 	elseif music['dream'].audio:isLooping() and inGhost then
-		fadeSound(music['dream'].audio)
+		fadeSound(music['dream'])
 	end
 end
 
