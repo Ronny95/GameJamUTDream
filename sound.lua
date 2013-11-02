5b7072907510fc1238f087901b24329f6ffa49f6
RyFry <ryan.lorey@utexas.edu>
2013-11-02 15:34:54 -0500

    still trying to fix sound

--- sound.lua
+++ sound.lua
@@ -12,14 +12,44 @@ music = {
 }
 
 soundfx = {
-	['switch'] = nil,--createNewMusic('switch.mp3', 0.0)
-	['boulder'] = nil,--createNewMusic('boulder.mp3, 0.0')
-	['changeForm'] = nil,--createNewMusic('changeForm.mp3', 0.0)
-	['lever'] = nil
+	['boulder'] = love.audio.newSource('assets/audio/rockrollmono.wav'),
+	['changeForm'] = love.audio.newSource('assets/audio/ghoston.wav'),
+	['lever'] = love.audio.newSource('assets/audio/switch_2mono.wav'),
+	['solve'] = nil,--love.audio.newSource('assets/audio/solve.wav'),
+	['statue'] = love.audio.newSource('assets/audio/statue.wav')
 }
 
 function fadeSound(music_obj)
-	
+	if music_obj == music['real'] and music_obj.volume > 0.0 then
+		music_obj.volume = music_obj.volume - 0.05
+		music_obj.audio:setVolume(music_obj.volume)
+
+		local mus = music['dream']
+		if not mus.audio:isPlaying() then
+			love.audio.play(mus.audio)
+		end
+		mus.volume = mus.volume + 0.05
+		mus.audio:setVolume(mus.volume)
+
+		if music_obj.volume <= 0.0 then
+			music_obj.audio:stop()
+		end
+	elseif music_obj == music['dream'] and music_obj.volume > 0.0 then
+		music_obj.volume = music_obj.volume - 0.05
+		music_obj.audio:setVolume(music_obj.volume)
+
+		local mus = music['dream']
+		if not mus.audio:isPlaying() then
+			love.audio.play(mus.audio)
+		end
+		mus.volume = mus.volume + 0.05
+		mus.audio:setVolume(mus.volume)
+
+		if music_obj.volume <= 0.0 then
+			music_obj.audio:stop()
+		end
+	end
 end
+
 
 
